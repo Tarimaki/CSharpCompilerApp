44fd6c6c6ef02ddeea7649f35eace8f68cf6039f
Tarimaki <ryu.1152.toaru.taka@gmail.com>
2019-06-13 15:09:31 +0900

    ボタン、テキストボックスの追加 アセンブリファイルの変更,バージョン情報と言語を設定

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,30 +30,43 @@
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(51, 283);
+            this.textBox1.Location = new System.Drawing.Point(59, 255);
             this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
             this.textBox1.Size = new System.Drawing.Size(217, 22);
             this.textBox1.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(293, 283);
+            this.button1.Location = new System.Drawing.Point(291, 254);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(38, 22);
             this.button1.TabIndex = 1;
             this.button1.Text = "...";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.fileOpenClick);
+            this.button1.Click += new System.EventHandler(this.fileOpen_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(117, 293);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(124, 33);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "コンパイル開始";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.CompileStart_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(366, 367);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
@@ -67,6 +80,7 @@
 
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,14 +13,20 @@ namespace CSharpCompilerApp
 {
     public partial class Form1 : Form
     {
-        private String SorceFileName;
+        private String SorceFileName;      //コンパイルするファイル名
+        private String CompilerFileName;   //コンパイラー名
 
         public Form1()
         {
             InitializeComponent();
+
+            this.Text = "C#コンパイラ";
+            CompilerFileName = "C:\\Windows\\Microsoft.NET\\Framework\\v4.0.30319\\csc.exe";
         }
 
-        private void fileOpenClick(object sender, EventArgs e)
+
+        //コンパイルするファイルを選択する
+        private void fileOpen_Click(object sender, EventArgs e)
         {
             OpenFileDialog ofd = new OpenFileDialog();
             ofd.Filter = "C#ソースファイル(*.cs)|*.cs|すべてのファイル(*.*)|*.*";
@@ -27,8 +34,27 @@ namespace CSharpCompilerApp
             ofd.FilterIndex = 1;
             ofd.Title = "csファイルを選択してください";
 
-            if (ofd.ShowDialog() == DialogResult.OK) ;
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
                 SorceFileName = ofd.FileName;
+                textBox1.Text = SorceFileName;
+            }
+        }
+
+        private void CompileStart_Click(object sender, EventArgs e)
+        {
+            Process process = new Process();
+            process.StartInfo.FileName = CompilerFileName;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+
+            String command = SorceFileName;
+
+            process.StartInfo.Arguments = command;
+
+            process.Start();
+
+            MessageBox.Show(process.StandardOutput.ReadToEnd());
         }
     }
 }

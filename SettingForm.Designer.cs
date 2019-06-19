49b6c455549315d6507a0d54875aab29e336f9fd
Tarimaki <ryu.1152.toaru.taka@gmail.com>
2019-06-19 17:13:03 +0900

    設定ページの追加 コンパイラの設定

---
+++ SettingForm.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace CSharpCompilerApp
+{
+    partial class SettingForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.OpenFile = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.apply_button = new System.Windows.Forms.Button();
+            this.cancel_button = new System.Windows.Forms.Button();
+            this.ok_button = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(578, 314);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.OpenFile);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(570, 285);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "コンパイラー";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // OpenFile
+            // 
+            this.OpenFile.Location = new System.Drawing.Point(519, 241);
+            this.OpenFile.Name = "OpenFile";
+            this.OpenFile.Size = new System.Drawing.Size(26, 23);
+            this.OpenFile.TabIndex = 2;
+            this.OpenFile.Text = "...";
+            this.OpenFile.UseVisualStyleBackColor = true;
+            this.OpenFile.Click += new System.EventHandler(this.OpenFile_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.textBox1.Location = new System.Drawing.Point(53, 243);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(459, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // apply_button
+            // 
+            this.apply_button.Location = new System.Drawing.Point(491, 316);
+            this.apply_button.Name = "apply_button";
+            this.apply_button.Size = new System.Drawing.Size(75, 27);
+            this.apply_button.TabIndex = 1;
+            this.apply_button.Text = "適用";
+            this.apply_button.UseVisualStyleBackColor = true;
+            this.apply_button.Click += new System.EventHandler(this.apply_button_Click);
+            // 
+            // cancel_button
+            // 
+            this.cancel_button.Location = new System.Drawing.Point(410, 316);
+            this.cancel_button.Name = "cancel_button";
+            this.cancel_button.Size = new System.Drawing.Size(75, 27);
+            this.cancel_button.TabIndex = 2;
+            this.cancel_button.Text = "キャンセル";
+            this.cancel_button.UseVisualStyleBackColor = true;
+            this.cancel_button.Click += new System.EventHandler(this.cancel_button_Click);
+            // 
+            // ok_button
+            // 
+            this.ok_button.Location = new System.Drawing.Point(329, 316);
+            this.ok_button.Name = "ok_button";
+            this.ok_button.Size = new System.Drawing.Size(75, 27);
+            this.ok_button.TabIndex = 3;
+            this.ok_button.Text = "OK";
+            this.ok_button.UseVisualStyleBackColor = true;
+            this.ok_button.Click += new System.EventHandler(this.ok_button_Click);
+            // 
+            // SettingForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(578, 352);
+            this.Controls.Add(this.ok_button);
+            this.Controls.Add(this.cancel_button);
+            this.Controls.Add(this.apply_button);
+            this.Controls.Add(this.tabControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "SettingForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "設定";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button apply_button;
+        private System.Windows.Forms.Button cancel_button;
+        private System.Windows.Forms.Button ok_button;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button OpenFile;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
---
+++ SettingForm.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharpCompilerApp
+{
+    public partial class SettingForm : Form
+    {
+        public SettingForm(Form form)
+        {
+            InitializeComponent();
+            InitializeValue();
+            
+            
+        }
+
+        private void InitializeValue()
+        {
+            //textbox
+            if (File.Exists(Form1.CompilerFile))
+            {
+                textBox1.Text = Form1.CompilerFile;
+            }
+            else
+            {
+                textBox1.Text = Form1.NOT_SETTING_MESSAGE;
+            }
+            
+
+            //label
+            label1.Text = "コンパイラーの設定をします。csc.exeファイルを選んでください。\n\n" +
+                "csc.exe以外のc#コンパイラーには対応していません。";
+        }
+
+        private void ok_button_Click(object sender, EventArgs e)
+        {
+            apply_button_Click(sender, e);
+            Dispose();
+        }
+
+        private void cancel_button_Click(object sender, EventArgs e)
+        {
+            Dispose();
+        }
+
+        private void apply_button_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //csc.exeの指定
+        private void OpenFile_Click(object sender, EventArgs e)
+        {
+            string filter = "csc.exe|csc.exe";
+            string title = "コンパイラーを選択してください。";
+            string FileName = Form1.CompileFileDialog(filter,title);
+
+            if (FileName == Form1.NOT_SETTING_MESSAGE)
+            {
+                Form1.CompilerFile = null;
+                textBox1.Text = Form1.NOT_SETTING_MESSAGE;
+            }
+            else
+            {
+                Form1.CompilerFile = FileName;
+                textBox1.Text = FileName;
+            }
+        }
+    }
+}
